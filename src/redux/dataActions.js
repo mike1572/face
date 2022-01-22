7618ec2029c22dd736b95fff7fb139c1ecee9fc8
CryoTechnic <cryogen65@gmail.com>
2022-01-22 10:45:16 -0500

    more code prep for boundary detection

--- src/redux/dataActions.js
+++ src/redux/dataActions.js
@@ -18,12 +18,15 @@ export const getHomePage = (dispatch) => {
     })
 }
 
-const MODEL_URL = '/models'
-const input = document.getElementById('video')
+export const loadModels = async () => {
+    const MODEL_URL = '/models'
+    const input = document.getElementById('video')
 
-await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
-await faceapi.loadFaceLandmarkModel(MODEL_URL)
-await faceapi.loadFaceRecognitionModel(MODEL_URL)
-let fullFaceDescriptions = await faceapi.detectAllFaces(input).withFaceLandmarks().withFaceDescriptors()
-fullFaceDescriptions = faceapi.resizeResults(fullFaceDescriptions, { width: input.width, height: input.height })
-faceapi.draw.drawDetections(input, fullFaceDescriptions)
+    await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+    await faceapi.loadFaceLandmarkModel(MODEL_URL)
+    await faceapi.loadFaceRecognitionModel(MODEL_URL)
+    let fullFaceDescriptions = await faceapi.detectAllFaces(input).withFaceLandmarks().withFaceDescriptors()
+    fullFaceDescriptions = faceapi.resizeResults(fullFaceDescriptions, { width: input.width, height: input.height })
+
+    return faceapi.draw.drawDetections(input, fullFaceDescriptions)
+}
