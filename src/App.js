6a5fb391efe0d7f124a2a0efcefce8bf9674869b
CryoTechnic <cryogen65@gmail.com>
2022-01-22 23:37:13 -0500

    added dark mode theme (prelim)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 import './App.css';
 
 
@@ -16,42 +16,80 @@ import store from './redux/store';
 // MUI
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import createTheme from '@mui/material/styles/createTheme';
-
-let theme = createTheme({
-  
+import Brightness6Icon from '@mui/icons-material/Brightness6';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import { IconButton } from '@mui/material';
+export const light = {
   palette: {
+    type: 'light',
     primary: {
-      // blue, text white
-      main: '#002984',
-      contrastText: '#fff'
+      main: '#3E065F',
+      contrastText: '#fff',
     },
-    secondary: {
-      // red , text black
-      main: '#f44336',
-      contrastText: '#00000'
+  },
+}
+
+export const dark = {
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#93FFD8',
+      contrastText: '#fff',
     },
-    warning: {
-      // red , text black
-      main: '#00000',
-      contrastText: '#fff'
-    }
-  }
+  },
+}
+
+
+
+
+// let theme = createTheme({
   
-})
+//   palette: {
+//     primary: {
+//       // blue, text white
+//       main: '#002984',
+//       contrastText: '#fff'
+//     },
+//     secondary: {
+//       // red , text black
+//       main: '#f44336',
+//       contrastText: '#00000'
+//     },
+//     warning: {
+//       // red , text black
+//       main: '#00000',
+//       contrastText: '#fff'
+//     }
+//   }
+  
+// })
 
 function App() {
+  const [theme, setTheme] = useState(true);
+  const icon = !theme ? <Brightness6Icon /> : <Brightness4Icon />;
+  const appliedTheme = createTheme(theme ? light : dark);
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={appliedTheme}>
     <Provider store={store}>
       <Fragment>
-        <Header/>
+        <Header>
+        <IconButton
+            edge="end"
+            color="inherit"
+            aria-label="mode"
+            onClick={() => setTheme(!theme)}
+          >
+            {icon}
+          </IconButton>
+        </Header>
         <Home/>
         <Recom/>
       </Fragment>
     </Provider>
     </ThemeProvider>
-  
   );
 }
+
+
 
 export default App;
